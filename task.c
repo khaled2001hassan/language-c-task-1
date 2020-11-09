ee27952171dab21178ecd93b7596175025f4dcde
khaled hassan <khaled2001hassan@gmail.com>
2020-11-09 13:06:56 +0300

    Create task.c

---
+++ task.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    float dol = 15.66;
+    int Pound;
+    printf("inter int number");
+    scanf("%x",&Pound);
+    float res ;
+    res=dol*Pound;
+    printf("The resulting is res=%f",res);
+
+
+    return 0;
+}
